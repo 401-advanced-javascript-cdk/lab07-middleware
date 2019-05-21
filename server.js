4691d82723912e3ab0d540097cb22100f4094ed4
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-21 15:37:32 -0700

    finish tasks

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 'use strict';
 
 const express = require('express');
-
 const app = express();
+const router = require('./lib/route.js');
+
+const errorHandler = require('./lib/middleware/error-handler.js');
+const notFound = require('./lib/middleware/notFound.js');
+const doMath = require('./lib/middleware/doMath.js');
+const requestTime = require('./lib/middleware/requestTime.js');
+const logger = require('./lib/middleware/logger.js');
 
 const PORT = process.env.PORT || 8080;
+app.use(requestTime);
+app.use(logger);
+app.use('/', router);
 
 app.get('/a', (req,res) => {
   res.status(200).send('Route A');
 });
 
-app.get('/b', (req,res) => {
-  res.status(200).send('Route B');
-});
-
-app.get('/c', (req,res) => {
-  res.status(200).send('Route C');
-});
-
-app.get('/d', (req,res) => {
-  res.status(200).send('Route D');
+app.get('/b/:num', (req, res) => {
+  req.number = doMath(parseInt(req.params.num));
+  res.status(200)
+  res.send(req.number.toString());
 });
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+app.get('/*', notFound);
+app.use(errorHandler);
