778c0f61bb285cbdfab463b1041068fce4078f53
Chris Kozlowski <kozlowskicd@gmail.com>
2019-05-21 18:03:23 -0700

    placeholder tests

--- __tests__/lib/middleware/requestTime.test.js
+++ __tests__/lib/middleware/requestTime.test.js
@@ -4,9 +4,9 @@ const time = require('../../../lib/middleware/requestTime.js');
 
 describe('requestTime.js', () => {
   it('attaches the current time to req.requestTime', () => {
-    let req = {requestTime: null};
-    time(req, {}, (err, req, res, next) => {
-      expect(req.requestTime).toBeInstanceOf(Number);
+    let request = {requestTime: null};
+    time(request, {}, (err, req, res, next) => {
+      expect(request.requestTime).not.toBeNull();
     });
   });
 });
